441bc440af7c89bbd3ae97bd6fa8127082d6777c
Anivesh00 <singhanivesh0@gmail.com>
2024-05-18 18:03:37 +0530

    Home page added

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';  // Import the Tailwind CSS file
+import "./index.css"; // Import the Tailwind CSS file
 import Stopwatch from "./components/Stopwatch";
-import Stopwatch2 from './components/Stopwatch2'
- 
+import Stopwatch2 from "./components/Stopwatch2";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/stop-watch" element={<Stopwatch />}>
-        </Route>
+        <Route path="/" element={<Stopwatch />}></Route>
       </Routes>
       <Routes>
-        <Route path="/stop-watch2" element={<Stopwatch2 />}>
-        </Route>
+        <Route path="/stop-watch" element={<Stopwatch />}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
- 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
