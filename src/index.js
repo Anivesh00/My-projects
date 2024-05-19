509e0574ff4a206f3a37ee3825fc589cfc815ebe
Anivesh00 <singhanivesh0@gmail.com>
2024-05-19 13:52:15 +0530

    bug fixed

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
-import ReactDOM from "react-dom/client";
+import React from "react";
+import { createRoot } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Stopwatch from "./components/Stopwatch";
 import Search from "./components/Search";
 
-export default function App() {
-  return (
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Search />}></Route>
-      </Routes>
-      <Routes>
-        <Route path="/stop-watch" element={<Stopwatch />}></Route>
-      </Routes>
-
-      <Routes>
-        <Route path="/search" element={<Search />}></Route>
+        <Route path="/" element={<Search />} />
+        <Route path="/stop-watch" element={<Stopwatch />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+  </React.StrictMode>
+);
